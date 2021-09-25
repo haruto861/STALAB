228c50f80f398b5f3a24fba16efbb442a01c09fc
haruto-matsushima <matsu_haruto@icloud.com>
2021-09-25 16:31:49 +0900

    add creatuser-method

--- STALAB/Classes/Modules/Signup/View/SignupViewController.swift
+++ STALAB/Classes/Modules/Signup/View/SignupViewController.swift
@@ -17,11 +17,15 @@ final class SignupViewController: UIViewController {
 
     @IBOutlet private weak var signupButton: UIButton! {
         didSet {
-            signupButton.addTarget(self, action: #selector(signup), for: .touchUpInside)
+            signupButton.addTarget(self, action: #selector(didTapSignup), for: .touchUpInside)
         }
     }
 
-    @objc private func signup() {
+    @objc private func didTapSignup() {
+        // 会員登録できないパターン
+        // 既にログインがある場合
+        // パスワードが不一致な場合
+        // メールアドレスの形式が正しくない場合
         guard
             let userName = userNameTextField.text ,
             let email = emailTextFiled.text,
@@ -33,14 +37,19 @@ final class SignupViewController: UIViewController {
         if password != confirm {
             return
         }
+         creatUser(email: email, password: password, userName: userName)
+    }
+
+    private func creatUser(email: String, password: String, userName: String) {
+
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {
                 print("認証情報の保存に失敗しました",error.localizedDescription)
                 return
+            } else {
             }
             print("認証情報の保存に成功しました")
             guard let uid = Auth.auth().currentUser?.uid else { return }
-            // Firestoreにデータを保存する
             Firestore.firestore().collection("users").document(uid).setData(["createdAt": Timestamp(),
                                                                              "userName": userName,
                                                                              "email": email
