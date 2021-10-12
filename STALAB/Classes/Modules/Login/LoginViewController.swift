01cdd97aadd8ca6d3c6a7fafa8d95c2340e9a290
haruto-matsushima <matsu_haruto@icloud.com>
2021-10-12 20:50:25 +0900

    add email login

---
+++ STALAB/Classes/Modules/Login/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  STALAB
+//
+//  Created by 松島悠人 on 2021/10/12.
+//
+
+import UIKit
+import Firebase
+
+final class LoginViewController: UIViewController {
+
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton! {
+        didSet {
+            loginButton.isEnabled = false
+            loginButton.addTarget(self, action: #selector(loginPressed(_:)), for: .touchUpInside)
+        }
+    }
+
+    @objc private func loginPressed(_ sender: UIButton) {
+        enableLoginButton()
+    }
+
+    private func enableLoginButton() {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty
+        else {
+            return
+        }
+        loginButton.isEnabled = true
+        login(email: email, password: password)
+    }
+
+    private func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                switch AuthErrorCode(rawValue: error._code) {
+                case .userNotFound:
+                    UIAlertController(title: "エラー", message: "ユーザーが見つかりません", preferredStyle: .alert).addCancel(handler: nil).addAction(title: "会員登録へ") { _ in
+                        Router.shared.toSignup(from: self)
+                    }.show(from: self)
+                case .networkError:
+                    UIAlertController.init(title: "ネットワークエラー", message: "電波のよりところで\n再度お試しください", preferredStyle: .alert).addOK(handler: nil).show(from: self)
+                case .invalidEmail:
+                    UIAlertController.init(title: "エラー", message: "メールアドレスの形式が違います", preferredStyle: .alert).addOK(handler: nil).show(from: self)
+                case .wrongPassword:
+                    UIAlertController(title: "エラー", message: "パスワードが違います", preferredStyle: .alert).addOK(handler: nil).show(from: self)
+                default:
+                    break
+                }
+            }
+        }
+    }
+}
