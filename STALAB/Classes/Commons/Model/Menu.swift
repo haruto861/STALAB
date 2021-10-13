7c4de9bb46b8ecb10ef32985d7277a27199eb340
haruto-matsushima <matsu_haruto@icloud.com>
2021-10-13 18:14:15 +0900

    add menumodel-content

--- STALAB/Classes/Commons/Model/Menu.swift
+++ STALAB/Classes/Commons/Model/Menu.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Content: Codable {
+    let menu: [Menu]
+}
+
+struct Menu: Codable {
+    let name: String
+    let size: [String]
+    let calorie: [String]
+
+    enum Menu: String, CodingKey {
+        case name = "name"
+        case size = "size"
+        case calorie = "calorie"
+    }
+}
