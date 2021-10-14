2698f2066c813ef3ef3cfd3237d89cc80beee696
haruto-matsushima <matsu_haruto@icloud.com>
2021-10-14 23:08:10 +0900

    success api-request

--- STALAB/Classes/Commons/Utility/API.swift
+++ STALAB/Classes/Commons/Utility/API.swift
@@ -15,27 +15,22 @@ protocol Request {
     // baseURLは固定だからextensionで実装する→リソースの所在地
     var baseURL: String { get }
     // 実行したい内容
-    var HTTPmethod:  Alamofire.HTTPMethod { get }
+    var HTTPmethod: Alamofire.HTTPMethod { get }
     // 欲しい内容
     var paramater: [String:Any] { get }
-
-}
-
-extension Request {
-    var baseURL: String {
-        let url = URL(string: "" ) as! String
-        return url
-    }
+    // ヘッダーに含める内容 key: value
+    var header: [String : String] { get }
 }
 
 class API {
+
     static var shared: API = .init()
     private init() {}
 
     //menuを取得するAPI
     func getMenu(completion: @escaping (Menu) -> Void) {
-//        AF.request("https://stalab.microcms.io/api/v1/stalabmenu281046861").responseJSON { res in
-//            print(res)
-//        }
+        AF.request("https://stalab.microcms.io/api/v1/stalabmenu281046861", method: .get, headers:[ "X-API-KEY" : "b5fc82a4-a500-48bf-8e43-adff446197cb" ]).responseJSON { res in
+            print(res)
+        }
     }
 }
--- STALAB/Classes/Modules/Menu/MenuViewController.swift
+++ STALAB/Classes/Modules/Menu/MenuViewController.swift
@@ -7,23 +7,12 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+final class MenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        API.shared.getMenu { menu in
+            print("メニュー",menu)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
